30e11b41fb689f23c0560cc5200b2cff891a41f0
ammbr <massaomaebara@gmail.com>
2017-07-26 17:45:39 -0300

    criando gráfici de barra

--- mismatch/mymismatch.php
+++ mismatch/mymismatch.php
@@ -10,10 +10,11 @@ $data = mysqli_query($conecta, $query)
 	or die("erro ao acessar banco de dados.");
 
 if (mysqli_fetch_array($data) != 0) {
-	$query = "SELECT mr.response_id, mr.topic_id, mr.response, mt.name AS topic_name FROM mismatch_response AS mr INNER JOIN mismatch_topic AS mt USING(topic_id) WHERE mr.user_id = '".$_SESSION['user_id']."'";
+	$query = "SELECT mr.response_id, mr.topic_id, mr.response, mt.name AS topic_name, me.name AS category_name FROM mismatch_response AS mr INNER JOIN mismatch_topic AS mt USING (topic_id) INNER JOIN mismatch_category AS me USING (category_id) WHERE mr.user_id = '".$_SESSION['user_id']."'";
 	$data = mysqli_query($conecta, $query)
 		or die("erro ao acessar banco de dados.");
 	$user_responses = array();
+	$mismatch_categories = array();
 	while($row = mysqli_fetch_array($data)) {
 		array_push($user_responses, $row);
 	}
@@ -36,11 +37,21 @@ if (mysqli_fetch_array($data) != 0) {
 		}
 		$score = 0;
 		$topics = array();
+		$categories = array();
 		for ($i = 0; $i < count($user_responses); $i++) {
 			if ((int)$user_responses[$i]['response'] + (int)$mismatch_responses[$i]['response'] == 3) {
 			$score += 1;
 			array_push($topics, $user_responses[$i]['topic_name']);
+			array_push($categories, $user_responses[$i]['category_name']);
 	        }
+		}
+		$category_totals = array(array($categories[0],0));
+		foreach ($categories as $category) {
+			if ($category_totals[count($category_totals) - 1][0] != $category) {
+				array_push($category_totals, array($category, 1));
+			} else {
+				$category_totals[count($category_totals) - 1][1]++;
+			}
 		}
 		if ($score > $mismatch_score) {
 			$mismatch_score = $score;
@@ -73,6 +84,9 @@ if (mysqli_fetch_array($data) != 0) {
 			foreach ($mismatch_topics as $topic) {
 				echo $topic . '<br>';
 			}
+			echo '<h4>Detalhes das categorias desencontradas:</h4>';
+			draw_bar_graph(400, 300, $category_totals, 5, UPLOADPATH.$_SESSION['user_id'].'-mymismatchgraph.png');
+			echo '<img src="'.UPLOADPATH.$_SESSION['user_id'].'-mymismatchgraph.png" alt="Mismatch Category graph"><br>';
 			echo '<h4>View <a href="view-profile.php?user_id='.$mismatch_user_id.'">'.$row['first_name'].'\'s profile</a>.</h4>';
 		}
 	}
@@ -81,3 +95,26 @@ if (mysqli_fetch_array($data) != 0) {
 	echo '<p> Você precisa primeiro <a href="questionnaire.php"> responder o questionário </a> antes de encontrar seu par imperfeito</p>';
 }
 include_once('footer.php');
+
+function draw_bar_graph($width, $height, $data, $max_value, $filename) {
+	$img = imagecreatetruecolor($width, $height);
+
+	$bg_color = imagecolorallocate($img, 255, 255, 255);
+	$text_color = imagecolorallocate($img, 255, 255, 255);
+	$bar_color = imagecolorallocate($img, 0, 0, 0);
+	$border_color = imagecolorallocate($img, 192, 192, 192);
+
+	imagefilledrectangle($img, 0, 0, $width, $height, $bg_color);
+
+	$bar_width = $width / ((count($data) * 2) + 1);
+	for ($i = 0; $i < count($data); $i++) { 
+		imagefilledrectangle($img, ($i * $bar_width * 2) + $bar_width, $height, ($i * $bar_width * 2) + ($bar_width * 2), $height - (($height / $max_value) * $data[$i][1]), $bar_color);
+		imagestringup($img, 5, ($i * $bar_width * 2) + ($bar_width), $height - 5, $data[$i][0], $text_color);
+	}
+	imagerectangle($img, 0, 0, $width - 1, $height - 1, $border_color);
+	for ($i = 1; $i <= $max_value; $i++) { 
+		imagestring($img, 5, 0, $height - ($i * ($height / $max_value)), $i, $bar_color);
+	}
+	imagepng($img, $filename, 5);
+	imagedestroy($img);
+}
